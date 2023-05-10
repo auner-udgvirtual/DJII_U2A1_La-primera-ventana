911b08fe11ac59505242acfed5a67d0f4880b2ec
Auner Medina <auner.medina@udgvirtual.udg.mx>
2023-05-10 07:54:54 -0700

    4.6. UX

--- Unidad 4/Assets/Scripts/BallController.cs
+++ Unidad 4/Assets/Scripts/BallController.cs
@@ -11,6 +11,12 @@ public class BallController : MonoBehaviour
     private bool ignoreNextCollision;
     private Vector3 startPosition;
 
+    [HideInInspector]
+    public int perfectPass;
+    public float superSpeed = 8;
+    private bool isSuperSpeedActive;
+    public int perfectPassCount = 3;
+
     private void Start()
     {
         startPosition = transform.position;
@@ -24,11 +30,17 @@ public class BallController : MonoBehaviour
             return;
         }
 
-        DeathPart deathPart = collision.transform.GetComponent<DeathPart>();
-
-        if (deathPart)
+        if (isSuperSpeedActive && !collision.transform.GetComponent<GoalController>())
         {
-            GameManager.singleton.RestartLevel();
+            Destroy(collision.transform.parent.gameObject, 0.2f);
+        } else
+        {
+            DeathPart deathPart = collision.transform.GetComponent<DeathPart>();
+
+            if (deathPart)
+            {
+                GameManager.singleton.RestartLevel();
+            }
         }
 
         rb.velocity = Vector3.zero;
@@ -36,6 +48,19 @@ public class BallController : MonoBehaviour
 
         ignoreNextCollision = true;
         Invoke("AllowNextCollision", 0.2f);
+
+        perfectPass = 0;
+        isSuperSpeedActive = false;
+    }
+
+    private void Update()
+    {
+        if (perfectPass >= perfectPassCount && !isSuperSpeedActive)
+        {
+            isSuperSpeedActive = true;
+
+            rb.AddForce(Vector3.down * superSpeed, ForceMode.Impulse);
+        }
     }
 
     private void AllowNextCollision()
